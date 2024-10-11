ac4eb17e391393e7e96c6327198e1963d2f79a5e
JARPZEN <juanrp700@gmail.com>
2024-10-11 01:36:49 -0500

    primer-commit

---
+++ backend/src/main/java/culturemedia/View.java
@@ -0,0 +1,8 @@
+package backend.src.main.java.culturemedia;
+
+import backend.src.main.java.culturemedia.model.repository.RecordVideo;
+
+import java.time.LocalDateTime;
+
+public record View (String userFullName, LocalDateTime startPlayingtime, Integer age, RecordVideo video) {
+}
