95036084329a58531e19866b54d19619e012a544
JARPZEN <juanrp700@gmail.com>
2024-10-23 21:53:02 -0500

    commit_R9

--- backend/src/main/java/culturemedia/View.java
+++ backend/src/main/java/culturemedia/View.java
@@ -5,6 +5,9 @@ import backend.src.main.java.culturemedia.model.repository.RecordVideo;
 import java.time.LocalDateTime;
 
 public record View (String userFullName, LocalDateTime startPlayingtime, Integer age, RecordVideo video) {
+    public View(int i, String testView) {
+    }
+
     public String Video() {
         return userFullName;
     }
