78839838c9381da46e294d7e1c705630cdf3142a
JARPZEN <juanrp700@gmail.com>
2024-10-30 18:52:38 -0500

    R10-Adding–controller

---
+++ backend/src/main/java/culturemedia/controllers/CultureMediaServiceImpl.java
@@ -0,0 +1,10 @@
+package backend.src.main.java.culturemedia.controllers;
+
+import backend.src.main.java.culturemedia.model.repository.RecordVideo;
+
+import java.util.List;
+
+public class CultureMediaServiceImpl {
+    public List<RecordVideo> findAll() {
+    return null; }
+}
