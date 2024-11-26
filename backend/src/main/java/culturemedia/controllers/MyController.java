a548e66febaf881a402f0c5d91146fb83a0f5f7f
JARPZEN <juanrp700@gmail.com>
2024-11-25 23:21:31 -0500

    front

--- backend/src/main/java/culturemedia/controllers/MyController.java
+++ backend/src/main/java/culturemedia/controllers/MyController.java
@@ -1,9 +1,19 @@
 package backend.src.main.java.culturemedia.controllers;
+import backend.src.main.java.culturemedia.model.repository.RecordVideo;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/videos")
 public class MyController {
+
+    @PostMapping
+    public ResponseEntity<RecordVideo> addVideo(@RequestBody RecordVideo video) {
+        // lógica para agregar un video
+        return ResponseEntity.ok(video);
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
@@ -13,4 +23,5 @@ public class MyController {
     public String echoMessage(@RequestBody String message) {
         return "Echo: " + message;
     }
+
 }
